bd2f941b795c514e8b9d257ddb544410bb5df7af
ch3cknull <ch3cknull@outlook.com>
2021-10-20 22:55:12 +0800

    chore: update access

--- web/lua/utils/access.lua
+++ web/lua/utils/access.lua
@@ -1,12 +1,10 @@
-local ngx = ngx
+local ngx = require "ngx"
 local uri = ngx.var.uri
 
-local api = {'check', 'ranking', 'data'}
+local api = {'check', 'ranking', 'data', 'flush'}
 
 -- Path Limit
-local str, err = ngx.re.match(uri, "^/v1/api/(" .. table.concat(api, '|') .. ')')
-if str then
-    return
-else
+local str, err = ngx.re.match(uri, "^/v1/api/(" .. table.concat(api, '|') .. ')$')
+if not str[1] then
     ngx.exit(ngx.HTTP_FORBIDDEN)
 end
