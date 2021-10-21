a308d01e1f8aae30fd6aac9f9237d7351974ff2d
ch3cknull <ch3cknull@outlook.com>
2021-10-21 18:29:38 +0800

    feat: fix ali-cdn real-ip header

--- web/lua/hooks/useInfo.lua
+++ web/lua/hooks/useInfo.lua
@@ -3,9 +3,11 @@ local ngx = require('ngx')
 local _M = {}
 
 function _M.get_client_info()
+    local headers = ngx.req.get_headers()
+
     local data = {
-        ip = ngx.var.remote_addr,
-        user_agent = ngx.req.get_headers()['user-agent'] or '',
+        ip = headers['ali-cdn-real-ip'] or ngx.var.remote_addr,
+        user_agent = headers['user-agent'] or '',
         uri = ngx.var.uri
     }
     return data
