4ccd8433e664599abb3e4c1aee8c76b830de1a63
ch3cknull <ch3cknull@outlook.com>
2021-10-21 13:45:22 +0800

    chore: update config

--- web/lua/config.lua
+++ web/lua/config.lua
@@ -1,6 +1,24 @@
 local _M = {}
 
-_M.base_url = 'https://asoulcnki.asia/v1/api'
-_M.expire = 4800
+_M.requests = {
+    base_url = 'https://asoulcnki.asia/v1/api'
+}
+
+_M.cache = {
+    expire = 4000
+}
+
+_M.api = {
+    check = {
+        min_length = 10,
+        max_length = 1000
+    },
+    flush = {
+        secret = "114514"
+    },
+    data = {
+        secure_key = "114514"
+    }
+}
 
 return _M
